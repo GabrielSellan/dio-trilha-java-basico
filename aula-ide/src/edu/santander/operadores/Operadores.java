52ecccbd8cb0fbceb077caa64da9cfd6f99f36c6
GabrielSellan <gabrielsellan2002@gmail.com>
2023-08-23 13:05:07 -0300

    Modulo Operadores completo

---
+++ aula-ide/src/edu/santander/operadores/Operadores.java
@@ -0,0 +1,29 @@
+package edu.santander.operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        
+        // Operadores
+
+        /*
+
+            Atribuição ( = )
+            Aritmédicos ( +, -, *, / )
+            Contatenação ( + em texto )
+            
+            Unários:
+            Incrementar ( ++ )
+            Negação ( ! )
+
+            Ternário: <Condição> ? <Caso seja verdadeiro> : <Caso seja falso>
+
+            Lógicos:
+            E ( && )
+            OU ( || )
+
+         */
+
+
+
+    }
+}
