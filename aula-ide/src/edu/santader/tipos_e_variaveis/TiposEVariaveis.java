c56e831022a12e1b535bd8dcacef28e9a8b35b8c
GabrielSellan <gabrielsellan2002@gmail.com>
2023-08-22 22:15:05 -0300

    Modulo Tipos e Variaveis completo

--- aula-ide/src/edu/santader/tipos_e_variaveis/TiposEVariaveis.java
+++ aula-ide/src/edu/santader/tipos_e_variaveis/TiposEVariaveis.java
@@ -2,6 +2,18 @@ package edu.santader.tipos_e_variaveis;
 
 public class TiposEVariaveis {
     public static void main(String[] args) {
-        System.out.println("fweefwe");
+        
+        // Variáveis
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 3635498987L;
+        float pi = 3.14F;
+        double salario = 2500.33;
+
+        // Constantes (final)
+        final double PI = 3.14;
+        
+
     }
 }
