cf7586cbc52d501ae9508fab784545c0b47aeff9
GabrielSellan <gabrielsellan2002@gmail.com>
2023-08-25 21:22:07 -0300

    Arrumando repositorio e add aula controle-fluxo

---
+++ aulas-santander/anatomiaDeClasses/Aula.java
@@ -0,0 +1,31 @@
+package edu.santander.anatomiaDeClasses;
+
+public class Aula {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Ola turma, sejam bem-vindos!");
+		
+		// Variavel
+		//String meuNome = "Gabriel";
+		//int anoFabricacao = 2022;
+		//boolean verdadeira = false;
+	
+		//anoFabricacao = 2018;
+		
+		// Método
+		String primeiroNome = "Gabriel";
+		String segundoNome = "Sellan";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto);
+		
+		
+}
+	
+	// Método
+	public static String nomeCompleto(String primeiroNome, String segundoNome) {
+		return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+	}
+	
+}
